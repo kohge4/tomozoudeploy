0c93704d4c8b7a2af89555d2d36e224b5340e4c6
kohge4 <robertstoko@gmail.com>
2020-04-12 22:42:19 +0900

    cd

--- tomozou/main.go
+++ tomozou/main.go
@@ -88,6 +88,7 @@ func main() {
 			devUserRepo.DB.Find(&track)
 			c.JSON(200, track)
 		})
+
 		rDev.GET("/tracktag", func(c *gin.Context) {
 			track := []domain.UserTrackTag{}
 			devUserRepo.DB.Find(&track)
