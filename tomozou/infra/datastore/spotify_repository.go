e51978cb4e762affd623c98bd457cdb2c77aac2e
kohge4 <robertstoko@gmail.com>
2020-04-20 13:52:53 +0900

    nowplayng edit v8

--- tomozou/infra/datastore/spotify_repository.go
+++ tomozou/infra/datastore/spotify_repository.go
@@ -134,7 +134,7 @@ func (repo *SpotifyItemDBRepository) ReadUserTrackTagByTagName(tagName string) (
 func (repo *SpotifyItemDBRepository) ReadUserTrackTagByUserIDANDTagName(userID int, tagName string) ([]domain.UserTrackTag, error) {
 	// nowplaying の 表示用
 	userTrackTags := []domain.UserTrackTag{}
-	repo.DB.Where("user_id = ? AND tag_name = ?", userID, tagName).Find(userTrackTags)
+	repo.DB.Where("user_id = ? AND tag_name = ?", userID, tagName).Find(&userTrackTags)
 	if len(userTrackTags) == 0 {
 		return userTrackTags, fmt.Errorf("nil error")
 	}
